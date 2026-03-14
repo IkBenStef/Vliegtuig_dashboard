0250c81807ead4861bc663d4ffc3d75a95e0cf2f
Stef <66081006+IkBenStef@users.noreply.github.com>
2026-03-15 00:46:52 +0100

    Update 2_✈️🌷_Schiphol_vluchten.py

--- pages/2_✈️🌷_Schiphol_vluchten.py
+++ pages/2_✈️🌷_Schiphol_vluchten.py
@@ -127,4 +127,4 @@ with b2:
         st.text('nog meer info')
         st.text('nog meer info')
 
-with st.expander('ruew vluchtdata'): st.dataframe(vlucht)
+with st.expander('ruwe vluchtdata'): st.dataframe(vlucht)
