0fe897c9489fa4443559131124cdc49d185ae105
Stef <66081006+IkBenStef@users.noreply.github.com>
2026-03-16 18:12:47 +0100

    Add files via upload

---
+++ haversine.py
@@ -0,0 +1,15 @@
+def haversine(lat1, lon1, lat2, lon2):
+    lat1 = np.radians(lat1)
+    lat2 = np.radians(lat2)
+    lon1 = np.radians(lon1)
+    lon2 = np.radians(lon2)
+
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a))
+
+    R = 6378137  # equatoriale radius (WGS84)
+    
+    return R * c
